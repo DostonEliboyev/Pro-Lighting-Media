39d2047b0679acab683fea9175ee120c29b04177
dostonhack <you@example.com>
2022-06-07 09:14:39 +0100

    add seo2

--- pages/drawing.js
+++ pages/drawing.js
@@ -24,7 +24,7 @@ function Drawing() {
               <meta property="og:description" content={`${blogPost.h4}`} />
               <meta property="og:url" content="https://www.plm.uz/" />
               <meta property="og:type" content="website" />
-              <link ref="icon" href="/favicon.ico" />
+              <link ref="icon" href="/favicon.ico /" />
             </Head>
           );
         })}
