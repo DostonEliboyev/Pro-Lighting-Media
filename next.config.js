3407676cd53e134705710d507f93062019e00003
dostonhack <you@example.com>
2022-06-27 14:04:00 +0100

    27.06.2022

--- next.config.js
+++ next.config.js
@@ -2,18 +2,18 @@
 const nextConfig = {
   i18n: {
     // providing the locales supported by your application
-   locales: ["en", "ru", "uz"],
-   
-   //  default locale used when the non-locale paths are visited
-   defaultLocale: "ru",
+    locales: ["en", "ru", "uz"],
+
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: "ru",
   },
   reactStrictMode: true,
   images: {
     domains: ['assets.reqres.in', "youtube.com"],
-    swcMinify:false, // it should be false by default 
+    swcMinify: false, // it should be false by default 
   },
 
-  
+
 }
 
 module.exports = nextConfig
--- pages/product/[characterId].js
+++ pages/product/[characterId].js
@@ -4,18 +4,31 @@ import Link from "next/link"
 import Head from "next/head"
 import Image from "next/image";
 import xIcon from "../../asset/img/xIcon.svg"
+import ProCircleGold from "../../components/proCircleGold/index"
 import { product1, product2, product3, product4, product5, product6 } from '../../public/lang/products.json'
 let Fulldata = [...product1, ...product2, ...product3, ...product4, ...product5, ...product6]
 
 export default function Character({ character }) {
-  console.log('character,',character);
-  const { locale, locales, asPath } = useRouter();
 
+  const language = {
+    en: "English",
+    "ru": "Russian",
+    "uz": "uzbekitan"
+  }
+  const { locale, isFallback } = useRouter();
+ 
   let random = Math.floor(Math.random() * 96)
+  if (isFallback) {
+    return <div className={styles.Loading}>
+              <div>
+                <div style={{height:'100%'}}>  <ProCircleGold /></div>
+                <div className={styles.Loading_p}><p>Loading...</p></div>
+              </div>
+            </div>
+  }
   return (
     <div className={styles.container}>
-      {character
-        .filter((p) => p.locale === locale)
+      {character?.filter((p) => p.locale === locale)
         .map((blogPost, i, ref) => {
           return (
             <>
@@ -31,7 +44,7 @@ export default function Character({ character }) {
 
               <div
                 className={styles.card}
-                key={i + 3}
+                key={blogPost?.title}
               >
                 <span className={styles.closeIcon} key={i + 1}>
                   <Link href="/product" innerRef={ref}>
@@ -41,7 +54,7 @@ export default function Character({ character }) {
 
                 <div className={styles.card_left}>
                   <h3>{blogPost.title}</h3>
-                  <Image src={`${blogPost.image}`} height={300} width={300} alt={blogPost.title} />
+                  <Image src={`${blogPost?.image}`} height={300} width={300} alt={blogPost.title} />
                 </div>
                 <div className={styles.card_right}>
                   <h4>{blogPost.script}</h4>
@@ -54,59 +67,52 @@ export default function Character({ character }) {
           );
         })}
 
-      <div className={styles.offerProduct}>       
-        <Link href={`/product/${Fulldata[random]?.title}`}>
+      <div className={styles.offerProduct}>
+        <Link href={`/product/${Fulldata[random]?.link}`}>
           <div className={styles.offerCard}>
-            <Image src={Fulldata[random]?.image} layout='fill' alt={Fulldata[random+9]?.title} />
+            <Image src={Fulldata[random]?.image} layout='fill' alt={Fulldata[random]?.title} />
             <h3>{Fulldata[random]?.title}</h3>
           </div>
         </Link>
-        <Link href={`/product/${Fulldata[random]?.title}`}>
+        <Link href={`/product/${Fulldata[random]?.link}`}>
           <div className={styles.offerCard}>
-            <Image src={Fulldata[random+3]?.image} layout='fill' alt={Fulldata[random+9]?.title} />
-            <h3>{Fulldata[random+3]?.title}</h3>
+            <Image src={Fulldata[random + 3]?.image} layout='fill' alt={Fulldata[random + 3]?.title} />
+            <h3>{Fulldata[random + 3]?.title}</h3>
           </div>
         </Link>
-        <Link href={`/product/${Fulldata[random]?.title}`}>
+        <Link href={`/product/${Fulldata[random]?.link}`}>
           <div className={styles.offerCard}>
-            <Image src={Fulldata[random+6]?.image} layout='fill' alt={Fulldata[random+9]?.title} />
-            <h3>{Fulldata[random+6]?.title}</h3>
+            <Image src={Fulldata[random + 6]?.image} layout='fill' alt={Fulldata[random + 6]?.title} />
+            <h3>{Fulldata[random + 6]?.title}</h3>
           </div>
         </Link>
-        <Link href={`/product/${Fulldata[random]?.title}`}>
+        <Link href={`/product/${Fulldata[random]?.link}`}>
           <div className={styles.offerCard}>
-            <Image src={Fulldata[random+9]?.image} layout='fill' alt={Fulldata[random+9]?.title} />
-            <h3>{Fulldata[random+9]?.title}</h3>
+            <Image src={Fulldata[random + 9]?.image} layout='fill' alt={Fulldata[random + 9]?.title} />
+            <h3>{Fulldata[random + 9]?.title}</h3>
           </div>
         </Link>
-
       </div>
-
-
     </div>
   )
 }
 
-export async function getStaticProps({ params }) {
-
-
+export async function getStaticProps({ params, locale, locales }) {
   const characterId = params.characterId
-  // const results = await fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters?name=${characterId}`).then(res => res.json());
-  // const characterId = params.characterId.replace(/\-/g, '+')
-  const results = Fulldata.filter(item => item.title.toLowerCase() === characterId.toLowerCase())
+  const results = Fulldata.filter(item => item.link.toLowerCase() === characterId.toLowerCase())
   return {
     props: {
-      character: results
+      character: results,
+      locale,
+      locales
     }
   }
 }
 
 export async function getStaticPaths() {
-  // const characters = await fetch('https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=500').then(res => res.json());
-
 
   const paths = Fulldata.map(character => {
-    const characterId = character.title;
+    const characterId = character.link;
     return {
       params: {
         characterId
@@ -116,6 +122,6 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }   
